507c4a3c50cc2fe6aa7bacf9d5150661de2f4c3e
Vishnu Sarukkai <sarukkai@stanford.edu>
2020-05-21 23:28:56 -0700

    Pretty good text filters working, or at least a starting point

--- app/scripts/getPagesSource.js
+++ app/scripts/getPagesSource.js
@@ -11,7 +11,7 @@ function processText(text){
     var lines = text.split("\n");
     var newText = "";
     for(i = 0; i < lines.length; i++){
-        if(lines[i].split(" ").length > 20){
+        if(lines[i].split(" ").length > 20){ // Try to make sure 20 words in line?? Why? Alex prob saw something when writing this. Add a condition to make sure total length of line < 200? No lines seem too long (will put in diff part)
             newText += lines[i];
         }
     }
@@ -37,19 +37,43 @@ function highlightText(wordQuery){
 
 // Let's have a dictionary of regex for each ToS element, start with 6 most impt to highlight
 // Making a txt file with common strings for each of these terms from TOSDR page
-/*
-var filterDict = {
-    govt: // Disclose info to the government: "enforcement" "government request" "lawful interception" "subpoena"
-    track: // Track you on other websites "DNT" header (Do Not Track), "track"
-    share: // Shares data with 3rd parties "share...information" "share...data"
-    sell: // Right to sell data in financial transactions
-    copyright: // Owns copyright on your content
-    court: // Waive right to go to court
-}*/
+var filterKeys = [
+    "govt", // Disclose info to the government: https://regex101.com/r/jpBIp1/2/tests
+    "track", // Track you on other websites "DNT" header (Do Not Track), "track" https://regex101.com/r/c2DOeX/2
+    "share", // Shares data with 3rd parties "share...information" "share...data" https://regex101.com/r/OQG4fv/2
+    "sell", // Right to sell data in financial transactions https://regex101.com/r/fzEr74/2
+    "copyright", // Owns copyright on your content https://regex101.com/r/STXxpi/1
+    "court" // Waive right to go to court https://regex101.com/r/UY04SZ/2
+]
+
+// Lookarounds are more concise, but slower for some of these, so avoiding using them for now
+var filterDict = [
+    /government request|(disclose.*legal|legal.*disclose)|subpoena|lawful interception|release/gmi, // (?=.*\bjack\b)(?=.*\bjames\b).* maybe use this lookaround syntax for disclose and legal, but it's slow!
+    /DNT|track|(record.*plugin|plugin.*record)/gmi,
+    /(shar.*(part|aggregate|anonymize))|((part|aggregate|anonymize).*shar)/gmi, // Lookaround version: /(?=.*shar.*)(?=.*(part|aggregate|anonymize).*)/gmi,
+    /((info|data|TOS|terms).*(merge|sale|sell|acqui|bankrupt|business|insolven|transfer))|((merge|sale|sell|acqui|bankrupt|business|insolven|transfer).*(info|data|TOS|terms))/gmi, // Lookaround version: 
+    /((stuff|content|submission|property).*(rights|license|property|copyright|reproduce|distribute|modify|owner))|((rights|license|property|copyright|reproduce|distribute|modify|owner).*(stuff|content|submission|property))/gmi,
+    /((waive|agree).*(court|arbitration|dispute|injuncti))|((court|arbitration|dispute|injuncti).*(waive|agree))/gmi
+]
+
+var filterLength = filterDict.length;
 
 // very basic test to isolate one sentence from instagram
 function filterSentence(sentence){
-    return ((sentence.search("govern") != -1) && (sentence.search("State") != -1));
+    if (sentence.length > 1000) {
+        return -1;
+    }
+
+    var toReturn = [];
+    for (var i = 0; i < filterLength; i++) {
+        if (filterDict[i].exec(sentence) !== null) {
+            console.log(filterKeys[i]);
+            console.log(sentence);
+            toReturn.push(i);
+        }
+    }
+    return toReturn;
+    // return ((sentence.search("govern") != -1) && (sentence.search("State") != -1));
 }
 
 // convert the document to plain text
@@ -78,9 +102,10 @@ function DOMtoString(document_root) {
     var sentences = []
     var lines = pageText.split("\n");
     for(i = 0; i < lines.length; i++){
-        var curSentences = lines[i].split(".");
+        var curSentences = lines[i].split(". "); // Need a space to not fail on things like "U.S."
         for(j = 0; j < curSentences.length; j++){
-            if(filterSentence(curSentences[j])){
+            var filters = filterSentence(curSentences[j]);
+            if(filters.length >= 0){
                 sentences.push(curSentences[j]);
                 highlightText(curSentences[j]);
             }
